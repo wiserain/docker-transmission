9177d3e008281f5119e3edd8042bc8f52ce3efcf
wiserain <mail275@gmail.com>
2018-09-29 11:33:03 +0800

    update transmission done script
    add folder strip

--- examples/script-torrent-done/revert_fake.sh
+++ examples/script-torrent-done/revert_fake.sh
@@ -7,12 +7,15 @@ FG_CMD="flexget -c /flexget/config.yml"
 TG_TOKEN='<TELEGRAM_BOT_TOKEN>'
 TG_ID="<TELEGRAM_CHAT_ID>"
 
+# manipulating variables
+[[ "${TR_TORRENT_DIR}" != */ ]] && TR_TORRENT_DIR="${TR_TORRENT_DIR}/" # add trailing slash if needed
+
 TG_URL='https://api.telegram.org/bot'$TG_TOKEN
 TG_URL=$TG_URL'/sendMessage?chat_id='$TG_ID
 
 read -r -d '' TG_MSG <<EOT
 *[NEXT] Download Reverted*
-${TR_TORRENT_NAME}
+_${TR_TORRENT_NAME}_
 EOT
 send_message() {
    res=$(curl --data-binary "text=${TG_MSG}" $TG_URL"&parse_mode=MarkDown")
@@ -26,9 +29,9 @@ EP_NUM=${EP_NUM#".E"}
 FNAMES=`${TR_CMD} --torrent $TR_TORRENT_ID --files | grep 100% | awk '{for(i=7;i<=NF;i++){printf "%s ", $i}; printf "\n"}'`
 # echo "${FNAMES}"
 
-for FNAME in "${FNAMES}"; do
-	FNAME="${TR_TORRENT_DIR}${FNAME%?}"
-	if [[ $FNAME == *"NEXT"* ]]; then
+while read -r FNAME; do
+	FNAME="${TR_TORRENT_DIR}${FNAME}"
+	if [[ $FNAME == *"NEXT"* ]] && [[ $FNAME == *"mp4"* ]]; then
 		ENCODER=$(mediainfo "${FNAME}" --Inform="General;%Encoded_Application%")
 		if [[ "$ENCODER" != "MH ENCODER" ]]; then
 			# rename before remove
@@ -42,10 +45,40 @@ for FNAME in "${FNAMES}"; do
 
 			# flexget series remove
 			FG_REMOVE_STATUS=`$FG_CMD series remove "${SERIES_NAME}" "${EP_NUM}"`
-			# echo "${FG_REMOVE_STATUS}"
+			# echo ${FG_REMOVE_STATUS}
 
 			# notification
 			send_message &> /dev/null
 		fi
 	fi
-done
+done <<< "${FNAMES}"
+
+# folder strip
+while read -r FNAME; do
+	DIR_FULL=$(dirname "$FNAME")
+	# echo "${DIR_FULL}"
+	DIR_ONLY=$(basename "$DIR_FULL")
+	# echo "${DIR_ONLY}"
+
+	FONLY=$(basename "$FNAME")
+	# echo $FONLY
+	FNAME_NOEXT=${FONLY%.*}
+	# echo $FNAME_NOEXT
+
+	if [[ "${DIR_ONLY}" == "${FNAME_NOEXT}" ]]; then
+		DIR_SIZE=$(du -hsb "${DIR_FULL}" | cut -f 1)
+		FSIZE=$(du -b "${FNAME}" | cut -f 1)
+
+		NFILES_TOTAL=$(ls -1q "${DIR_FULL}" | wc -l)
+		NFILES_INVAL=$(ls -1q "${DIR_FULL}"/*.{txt,nfo} 2>/dev/null | wc -l)
+		if ((${NFILES_TOTAL}-${NFILES_INVAL} <= 1)); then
+			# echo "${FNAME}"
+			mv "${FNAME}" "${DIR_FULL}/.."
+			rm -rf "${DIR_FULL}"
+
+			# remove torrent
+			# TR_REMOVE_STATUS=`${TR_CMD} --torrent $TR_TORRENT_ID --remove-and-delete`
+			# echo "${TR_REMOVE_STATUS}"
+		fi
+	fi
+done <<< "$(find "${TR_TORRENT_DIR}" -type f \( -name '*.mp4' -or -name '*.mkv' \))"
